d583a8bd0133f49ba9214f6a77852610f005cd92
Virtuosek <remagkes@gmail.com>
2019-03-21 00:38:09 +0100

    added maven javafx dep, imported fxml files, app is launchable

--- implementation/workspace/cassette-deck/src/main/java/model/Simulation.java
+++ implementation/workspace/cassette-deck/src/main/java/model/Simulation.java
@@ -2,20 +2,23 @@ package model;
 
 import java.util.ArrayList;
 import java.util.List;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
 public class Simulation {
 
-	private CassetteDeck deck;
-	private List<Cassette> cassettes;
-	
-	public Simulation() {
-		this.deck = new CassetteDeck();
-		this.cassettes = new ArrayList<>();
-	}
-	
-	public void start() {
-		while(true) {
-			
-		}
-	}
+    private CassetteDeck deck;
+    private List<Cassette> cassettes;
+
+    public Simulation() {
+        this.deck = new CassetteDeck();
+        this.cassettes = new ArrayList<>();
+    }
+
+    public void start() {
+
+    }
 }
--- implementation/workspace/cassette-deck/src/main/java/model/Start.java
+++ implementation/workspace/cassette-deck/src/main/java/model/Start.java
@@ -1,10 +1,38 @@
 package model;
 
-public class Start {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
 
-	public static void main(String[] args) {
-		Simulation simulation = new Simulation();
-		simulation.start();
-	}
+public class Start extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+        System.out.println("launched?");
+        Simulation simulation = new Simulation();
+        simulation.start();
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/cdLauncher.fxml")); //make sure fxml files are in src/main/resources, otherwise it won't work.
+        stage.setTitle("t");
+        stage.setScene(new Scene(root));
+        stage.show();
+
+        /* Testing if display works: */
+//        Button button = new Button();
+//        button.setText("Click");
+//        StackPane layout = new StackPane();
+//        layout.getChildren().add(button);
+//        Scene scene = new Scene(layout, 300, 250);
+//        stage.setScene(scene);
+//        stage.show();
+        System.out.println("Display?");
+    }
 
 }
